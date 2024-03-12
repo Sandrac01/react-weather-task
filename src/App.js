62c4915ff2e07f4e52b7e7f55ce2aae2fb11d3b4
Sandrac01 <152858277+Sandrac01@users.noreply.github.com>
2024-03-12 11:29:06 +0200

    created a new react folder, added a search engine and some css to style the page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './Search.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="weather-app">
+      <header>
+        <form class="search-form" id="search-form">
+          <input
+            type="search"
+            placeholder="Enter a city.."
+            required
+            id="search-form-input"
+            class="search-form-input"
+          />
+          <input type="submit" value="Search" class="search-form-button" />
+        </form>
       </header>
+      <main>
+        <div class="weather-app-data">
+          <div>
+            <div class="weather-app-city" id="city"></div>
+            <p class="weather-app-details">
+              <span id="time"></span>,<span id="description"></span>
+              <br />
+              Humidity: <strong id="humidity"></strong>, Wind:
+              <strong id="wind-speed"></strong>
+            </p>
+          </div>
+          <div class="weather-app-temperature-container">
+            <div id="icon"></div>
+            <div class="weather-app-temperature" id="temperature"></div>
+            <div class="weather-app-unit">°C</div>
+          </div>
+        </div>
+        <div class="weather-forecast" id="forecast"></div>
+      </main>
+
+      <footer>
+        This project was coded by
+        <a
+          href="https://github.com/Sandrac01/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Sandra Chinouya
+        </a>
+        , is
+        <a
+          href="https://github.com/Sandrac01/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+        and
+        <a
+          href="https://github.com/Sandrac01/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          hosted on Netlify
+        </a>
+      </footer>
     </div>
   );
 }
